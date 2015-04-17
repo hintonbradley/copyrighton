304ff4dff8769ef8b5699778e403b9db009df635
James Bradley <jamesbradley@Jamess-MacBook-Pro.local>
2015-04-16 19:07:26 -0700

    Added code for posting copyright info to profile

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 //ADDING LIBRARIES/MODULES/MIDDLEWARE TO APP://
 ///////////////////////////////////////////////
+
 //Creating an Express application:
 var express = require('express'),
 //Adding bodyParser Middleware:
 bodyParser = require('body-parser'),
-
+//Creating models to define how data is to be stored:
 db = require("./models"),
-//For API request?
+//Create ability to make API/http calls:
     request = require('request'),
 //In order to track sessions, express-session is required (command-line:npm install --save express-session):
     session = require("express-session"),
     app = express();
-//Adding key information for API:
-var env = process.env;
-//var api_key = env.MY_API_KEY;
+//Overrides the method of a request (for PUTS?)
 var methodOveride = require('method-override');
-
 app.use(methodOveride("_method"));
-
 //installed ejs to read html files under views/user directory:
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-
 //Defining the req.session (specific user who logs in):
 app.use(session({
   secret: 'Double Top Secret Probation',
@@ -29,18 +25,14 @@ app.use(session({
   save: {uninitialized: true
   }
 }))
-
 //Creating ability to track sessions:
 app.use("/", function (req, res, next) {
-
 //If user has not signed in, this code sets req.session.userId to null, otherwise, it will set to current user:
   req.session.userId = req.session.userId || null;
-
 
   req.login = function (user) {
     req.session.userId = user.id;
   };
-
   req.currentUser = function () {
     return db.User.find({
         where: {
@@ -52,14 +44,15 @@ app.use("/", function (req, res, next) {
         return user;
       })
   };
-
   req.logout = function () {
     req.session.userId = null;
     req.user = null;
   }
-
   next(); 
 });
+
+//CREATING GET REQUESTS
+///////////////////////
 
 //Creating get request for index page which renders the index file in views directory (static page):
 app.get("/", function (req, res) {
@@ -71,7 +64,7 @@ app.get('/signup', function(req,res){
   res.render("user/signup");
 });
 
-//Creating get request for signup page which renders the signup file in views/users directory (static page?):
+//Creating get request for signin page which renders the login file in views/users directory:
 app.get('/login', function(req,res){
   if((req.session.userId===null)||(req.session.userId===undefined)) {
     // If no user is currently logged in, then render the login page:
@@ -83,7 +76,7 @@ app.get('/login', function(req,res){
   }
 });
 
-//Creating get request for signup page which renders the signup file in views/users directory (static page?):
+//Creating get request for profile page which renders the signup file in views/users directory (static page?):
 app.get('/profile', function(req,res){
   if(req.session.userId === null) {
     // User is not logged in, so don't let them pass
@@ -93,9 +86,44 @@ app.get('/profile', function(req,res){
     req.currentUser().then(function(user){
       if (user) {
         user.getSongs().then(function(songs) {
-          res.render('user/profile', { user: user, songs: songs });
+          res.render('user/profile', { user: user, songs: songs});
         });
       }
+    });
+  }
+});
+
+
+app.get('/album/:id', function (req, res) {
+  var url = "https://api.spotify.com/v1/albums/" + req.params.id;
+  console.log("This is my url /n/n/n/n/n/n/n/" + url);
+  request(url, function(err, resp, body){
+    if (!err && resp.statusCode === 200) {
+        var jsonData = JSON.parse(body);
+//        var albumName = jsonData.tracks.href;
+  //      console.log("This is my albumName: "+ albumName);
+    }
+    res.render('album',{taco: jsonData, albumId: req.params.id, songTitle: req.query.songTitle });
+  });
+});
+
+
+
+
+app.post('/profile', function(req, res) {
+
+  var userId = req.session.userId || null;
+  var favorite = req.body.favorite;
+
+  if(!userId) {
+    res.redirect('/login');
+  } else {
+    db.Song.create({
+      UserId: req.session.userId,
+      song_title: favorite.songTitle,
+      album_id: favorite.albumId
+    }).then(function() {
+      res.redirect('/profile');
     });
   }
 });
@@ -157,7 +185,7 @@ app.get('/songs', function (req, res) {
     }
     console.log("This is my object data: " +jsonData);
     console.log(body);
-    res.render('songs',{taco: jsonData});
+    res.render('songs',{taco: jsonData, songTitle: req.query.songTitle});
   });
 });
 
@@ -172,7 +200,7 @@ app.get('/album/:id', function (req, res) {
 //        var albumName = jsonData.tracks.href;
   //      console.log("This is my albumName: "+ albumName);
     }
-    res.render('album',{taco: jsonData});
+    res.render('album',{taco: jsonData, albumId: req.params.id, songTitle: req.query.songTitle });
   });
 });
 
